2f2341b1e077698b1eba980da1b9aa5465f7b979
GiuseppeCriscione <g.criscione@innonation.it>
2025-09-05 22:47:09 +0200

    feat: Add SearchBar composable
    Adds a reusable `SearchBar` composable for implementing search functionality.
    
    Includes:
    - `SearchBar`: A `Card` composable containing a `TextField` for text input and an `Image` for the search icon.
        - Manages its own text state.
        - Triggers the `onSearch` callback when the search action is performed (either via keyboard or icon click).
        - Styled with custom colors and text styles from the theme.
        - Uses `debounceClickable` for the search icon to prevent multiple rapid clicks.
    - A `@Preview` function `SearchBarPreview` for `SearchBar`.

---
+++ app/src/main/java/it/datalux/homeworktest/presentation/common/bar/SearchBar.kt
@@ -0,0 +1,102 @@
+package it.datalux.homeworktest.presentation.common.bar
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import it.datalux.homeworktest.R
+import it.datalux.homeworktest.core.theme.backgroundSecondary
+import it.datalux.homeworktest.core.theme.ctaStyle
+import it.datalux.homeworktest.core.theme.onBackgroundSecondary
+import it.datalux.homeworktest.core.utils.debounceClickable
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit = { }
+) {
+    val (text, onTextChange) = remember { mutableStateOf("") }
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.backgroundSecondary,
+        ),
+        shape = RoundedCornerShape(size = 28.dp)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = text,
+                onValueChange = onTextChange,
+                modifier = Modifier.weight(1f),
+                placeholder = { Text("Search", style = ctaStyle) },
+                textStyle = ctaStyle,
+                maxLines = 1,
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    cursorColor = Color.onBackgroundSecondary,
+                ),
+
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        onSearch(text)
+                    }
+                )
+            )
+
+            Image(
+                painter = painterResource(R.drawable.icon_search),
+                contentDescription = "Search",
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(24.dp)
+                    .debounceClickable {
+                        onSearch(text)
+                    }
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SearchBarPreview() {
+    SearchBar()
+}
+
