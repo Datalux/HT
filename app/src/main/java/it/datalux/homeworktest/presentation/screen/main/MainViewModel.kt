8627014b9833be8bc909cc756dbfe74ec9320615
GiuseppeCriscione <g.criscione@innonation.it>
2025-09-07 15:52:04 +0200

    refactor: Remove unused imports from MainViewModel
    Removes unused imports from `MainViewModel.kt`:
    - `android.app.Application`
    - `android.content.Context`
    - `androidx.lifecycle.viewModelScope`
    - `dagger.hilt.android.qualifiers.ApplicationContext`
    - `it.datalux.homeworktest.core.shared.checkInternetConnection`
    - `it.datalux.homeworktest.core.utils.AppAlert`
    - `it.datalux.homeworktest.core.utils.InternetManager`
    - `kotlinx.coroutines.flow.MutableStateFlow`
    - `kotlinx.coroutines.flow.asStateFlow`
    - `kotlinx.coroutines.launch`

--- app/src/main/java/it/datalux/homeworktest/presentation/screen/main/MainViewModel.kt
+++ app/src/main/java/it/datalux/homeworktest/presentation/screen/main/MainViewModel.kt
@@ -1,17 +1,7 @@
 package it.datalux.homeworktest.presentation.screen.main
 
-import android.app.Application
-import android.content.Context
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
-import it.datalux.homeworktest.core.shared.checkInternetConnection
-import it.datalux.homeworktest.core.utils.AppAlert
-import it.datalux.homeworktest.core.utils.InternetManager
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
