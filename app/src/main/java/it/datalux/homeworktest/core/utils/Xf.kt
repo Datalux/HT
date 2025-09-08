1348c4bdea0db21358becc2117d3ba622bcc87b9
GiuseppeCriscione <g.criscione@innonation.it>
2025-09-08 11:49:46 +0200

    refactor: Make debounceClickable a Composable function
    Updates the `debounceClickable` Modifier extension function:
    - Annotates it with `@Composable`.
    - Uses `remember` to store `lastClickTime`.
    - Uses `rememberUpdatedState` for the `onClick` lambda to ensure the latest version is used.
    - Changes `crossinline onClick` to `onClick`.
    
    This change is made to properly manage state within the Composable context.

--- app/src/main/java/it/datalux/homeworktest/core/utils/Xf.kt
+++ app/src/main/java/it/datalux/homeworktest/core/utils/Xf.kt
@@ -1,6 +1,5 @@
 package it.datalux.homeworktest.core.utils
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,9 +8,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.util.Date
 
 fun String?.toStringOrEmpty() = this ?: ""
 fun Double?.toDoubleOrZero() = this ?: 0.0
@@ -20,15 +16,17 @@ fun Long?.toLongOrZero() = this ?: 0
 fun Int?.toIntOr(or: Int) = this ?: or
 fun Boolean?.toBooleanOrFalse() = this ?: false
 
-inline fun Modifier.debounceClickable(
+@Composable
+fun Modifier.debounceClickable(
     debounceInterval: Long = 1,
-    crossinline onClick: () -> Unit,
+    onClick: () -> Unit,
 ): Modifier {
-    var lastClickTime = 0L
-    return clickable() {
+    var lastClickTime by remember { mutableLongStateOf(0L) }
+    val currentOnClick by rememberUpdatedState { onClick() }
+    return clickable {
         val currentTime = System.currentTimeMillis()
         if ((currentTime - lastClickTime) < debounceInterval) return@clickable
         lastClickTime = currentTime
-        onClick()
+        currentOnClick()
     }
 }
